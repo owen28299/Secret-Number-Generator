54d2972b290beece6b53d20b609f3e9a1b546877
Owen <owenyangg@gmail.com>
2016-03-08 21:33:16 -1000

    Owen Finished

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,23 @@
 'use strict';
-module.exports = function() {
-  
+
+module.exports =  function secretNumberGenerator() {
+
+  var num = 0;
+
+  var setRandNum = function() {
+
+     if(num === 0){
+        num = Math.floor(Math.random()*1000001);
+        return num;
+     }
+     else {
+         return num;
+     }
+
+
+  };
+
+  return setRandNum;
+
+
 };
